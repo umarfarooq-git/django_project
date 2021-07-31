c31ef1099566510a2d511af1c071911290a13a4a
umar farooq <johndoe@example.com>
2021-07-31 06:11:29 +0500

    Temlates are added

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1> Blog about </h1>')
+    return render(request, 'blog/about.html')
 # Create your views here.
