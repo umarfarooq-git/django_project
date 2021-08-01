739f9f671b98621d64051688a455e50d6700800c
umar farooq <johndoe@example.com>
2021-08-02 02:54:42 +0500

    addition of bootstra template and CSS

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,14 @@ posts= [
 {   'author': 'umar farooq',
     'title': 'Post1',
     'content': 'first post content',
-    'date': '20 august 2021'
+    'date_posted': '20 august 2021'
 
 },
 {
     'author': 'irfan yousaf',
     'title': 'Post2',
     'content': 'second post content',
-    'date': '12 july 2021'
+    'date_posted': '12 july 2021'
 }
 ]
 
