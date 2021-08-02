c9d1635c4973fa588bb56fd8e6946afd7e5d68db
umar farooq <johndoe@example.com>
2021-08-03 04:58:04 +0500

    registeration of Post model at admin panel and cahnge in date format

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts= [
-{   'author': 'umar farooq',
-    'title': 'Post1',
-    'content': 'first post content',
-    'date_posted': '20 august 2021'
-
-},
-{
-    'author': 'irfan yousaf',
-    'title': 'Post2',
-    'content': 'second post content',
-    'date_posted': '12 july 2021'
-}
-]
 
 def home(request):
     context={'posts': Post.objects.all() }
