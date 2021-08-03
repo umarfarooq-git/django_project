65aa1bb238cd51196d759307998eee8f589fc8c3
umar farooq <johndoe@example.com>
2021-08-03 22:02:37 +0500

    Formation of User registration page

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
-
-# Create your views here.
+def register(request):
+    form=UserCreationForm()
+    form=UserCreationForm(request,'users/register.html', {'form':form})
