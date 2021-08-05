7e5e42279ac60f8c8596e0638f809005e1fa5e9c
umar farooq <johndoe@example.com>
2021-08-05 05:14:09 +0500

    profile template and routing

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/',user_view.register, name='register'),
+    path('profile/',user_view.profile, name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('lgout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
