b5ca5dbd0d8b4b82201770ba1d11edc2d700e925
umarfarooq-git <umaryousafkamboh@gmail.com>
2021-08-16 04:33:44 +0500

    user profile page updation

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -18,4 +18,10 @@ def register(request):
     return render(request,'users/register.html', {'form':form})
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    u_form=UserUpdateForm(instance=request.user)
+    p_form=ProfileUpdateForm(instance=request.user.profile)
+    context= {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request, 'users/profile.html',context)
