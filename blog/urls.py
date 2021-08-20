b86e5c1635af1ffc974c58526e6686c57f57a7ac
umarfarooq-git <umaryousafkamboh@gmail.com>
2021-08-20 23:18:01 +0500

    Updation, Deletion, Detail view of post and Navigation Bar updation

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostCreateview, postlistview, PostDetailview, CreateView
+from .views import PostCreateview, postlistview, PostDetailview, PostUpdateview, PostDeleteview
 
 urlpatterns =[
     path('', postlistview.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailview.as_view(), name='post-detail'),
     path('post/new/', PostCreateview.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateview.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteview.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 
@@ -26,9 +26,33 @@ class PostCreateview(LoginRequiredMixin, CreateView):
         form.instance.author=self.request.user
         return super().form_valid(form)
 
+class PostUpdateview(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model= Post  
+    fields=['title','content']
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
 
-    #def get_success_url(self):
-     #   return reverse('blog-home')
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteview(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+    model= Post
+    success_url= '/' #this is another way to manage success url. / denotes the home page.
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    
+
+    def get_success_url(self):
+        return reverse('blog-home')
     
     
 def about(request):
